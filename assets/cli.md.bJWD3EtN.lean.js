bb02c0b7de23571c3aff681f5fcbd701f96145f1
Rezougui Rayan Zin Eddine <rayan03rezougui@gmail.com>
2026-05-06 13:25:23 +0100

    Updates

---
+++ assets/cli.md.bJWD3EtN.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,o as a,c as s,ae as i}from"./chunks/framework.Bsyn99bI.js";const k=JSON.parse('{"title":"CLI Reference","description":"","frontmatter":{},"headers":[],"relativePath":"cli.md","filePath":"cli.md"}'),n={name:"cli.md"};function o(d,t,r,p,l,h){return a(),s("div",null,[...t[0]||(t[0]=[i("",40)])])}const g=e(n,[["render",o]]);export{k as __pageData,g as default};
